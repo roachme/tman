a4ff8f9642cb4842545dd5803057bcd4c7847651
roach <qcockroach6@gmail.com>
2024-07-11 13:55:55 +0300

    builtin/env: Refactor command env

--- src/builtin/env.lua
+++ src/builtin/env.lua
@@ -1,14 +1,76 @@
 local env = require("core.env")
-local core = require("core.core")
 local shell = require("aux.shell")
 local common = require("core.common")
 local taskid = require("core.taskid")
 local utils = require("aux.utils")
 local config = require("secondary.config")
---local help = require("secondary.help")
---local getopt = require("posix.unistd").getopt
 
 env.init(config.core.refs.envs)
+
+---Add new environment.
+---@param envname string
+local function _env_add(envname)
+    if not envname then
+        return common.die(1, "env name is required\n", "no envname")
+    end
+    if env.exists(envname) then
+        return common.die(1, "such env name already exists\n", envname)
+    end
+    env.add(envname, utils.get_input("description"))
+    shell.setcurr("")
+end
+
+---Delete an environment.
+---@param envname string
+local function _env_del(envname)
+    if not envname then
+        common.die(1, "no current environment\n", "env")
+    elseif not env.exist(envname) then
+        common.die(1, "no such environment\n", "env")
+    end
+
+    io.write("Do you want to continue? [Yes/No] ")
+    local confirm = io.read("*line")
+    if confirm ~= "Yes" then
+        print("deletion is cancelled")
+        os.exit(1)
+    end
+    -- roachme: TODO
+    -- delete taskid, taskunit and struct
+    env.del(envname)
+end
+
+---Switch to previous environment.
+---@param prevenv string?
+local function _env_prev(prevenv)
+    if not prevenv then
+        return common.die(1, "no previous env\n", "env")
+    end
+
+    env.setcurr(prevenv)
+    -- roachme: gotta structure it.
+    -- update task as well
+    taskid.init(config.core.refs.ids)
+    local curr_id = taskid.getcurr(prevenv)
+    if curr_id then
+        shell.setcurr(utils.genname(prevenv, curr_id))
+    else
+        shell.setcurr("")
+    end
+end
+
+---Switch to environment.
+---@param envname string
+local function _env_use(envname)
+    if not env.exists(envname) then
+        return common.die(1, "no such env name\n", envname)
+    end
+    env.setcurr(envname)
+
+    taskid.init(config.core.refs.ids)
+    local curr_id = taskid.getcurr(envname) or ""
+    shell.setcurr(utils.genname(envname, curr_id))
+end
 
 --- Define or display task environments.
 local function builtin_env()
@@ -16,62 +78,15 @@ local function builtin_env()
     local envname = arg[2]
 
     if cmd == "add" then
-        if not envname then
-            common.die(1, "env name is required\n", "no envname")
-        end
-        if env.exists(envname) then
-            common.die(1, "such env name already exists\n", envname)
-        end
-        env.add(envname, "auto generated description " .. envname)
-        core.init()
-
-        taskid.init(config.core.refs.ids)
-        local curr_id = taskid.getcurr(envname)
-        if curr_id then
-            shell.setcurr(utils.genname(envname, curr_id))
-        else
-            shell.setcurr("")
-        end
-    elseif cmd == "curr" then
-        print(env.getcurr())
+        _env_add(envname)
     elseif cmd == "del" then
-        envname = envname or env.getcurr()
-
-        io.write("Do you want to continue? [Yes/No] ")
-        local confirm = io.read("*line")
-        if confirm ~= "Yes" then
-            print("deletion is cancelled")
-            os.exit(1)
-        end
-        env.del(envname)
-    elseif not cmd or cmd == "list" then
+        _env_del(envname or env.getcurr())
+    elseif cmd == "list" then
         env.list()
     elseif cmd == "prev" then
-        local prev = env.getprev()
-
-        if not prev then
-            return common.die(1, "no previous env\n", "env")
-        end
-
-        env.setcurr(prev)
-        -- roachme: gotta structure it.
-        -- update task as well
-        taskid.init(config.core.refs.ids)
-        local curr_id = taskid.getcurr(prev)
-        if curr_id then
-            shell.setcurr(utils.genname(prev, curr_id))
-        else
-            shell.setcurr("")
-        end
+        _env_prev(env.getprev())
     elseif cmd == "use" then
-        if not env.exists(envname) then
-            return common.die(1, "no such env name\n", envname)
-        end
-        env.setcurr(envname)
-
-        taskid.init(config.core.refs.ids)
-        local curr_id = taskid.getcurr(envname) or ""
-        shell.setcurr(utils.genname(envname, curr_id))
+        _env_use(envname)
     else
         common.die(1, "no such env command\n", cmd)
     end
