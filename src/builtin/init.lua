d965862bb1403d74ae3286458360504e0fad930a
roach <qcockroach6@gmail.com>
2024-07-13 02:33:13 +0300

    init: Add support to download repos

--- src/builtin/init.lua
+++ src/builtin/init.lua
@@ -1,7 +1,20 @@
 local core = require("core.core")
+local git = require("secondary.git")
+local config = require("secondary.config")
 
 local function tman_init()
-    return core.create() and 0 or 1
+    local path = core.struct.code.path
+
+    if not core.create() then
+        core.die(1, "could not create core structure", "init")
+    end
+
+    for _, repo in pairs(config.user.repos) do
+        if not git.repo_clone(repo.link, repo.name, path) then
+            core.die(1, "could not download repo '%s'", "init", repo.name)
+        end
+    end
+    return 0
 end
 
 return tman_init
