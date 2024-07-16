9caf6e99fba764237108023607fb66e880f2d33b
roach <qcockroach6@gmail.com>
2024-07-16 10:51:51 +0300

    plugin/struct: Refactor code

--- src/plugin/struct.lua
+++ src/plugin/struct.lua
@@ -7,83 +7,58 @@ local config = require("struct.config")
 local core = require("core.core")
 
 local struct = {}
-
 local uconfig
 
--- Private functions: end --
-
---- Create dirs.
--- @param base directry structure base
+---Create dirs.
+---@param base string
 local function create_dirs(base)
     for _, dir in pairs(uconfig.struct.dirs) do
         utils.mkdir(base .. "/" .. dir)
     end
 end
 
---- Create files.
--- @param base file structure base
+---Create files.
+---@param base string
 local function create_files(base)
     for _, file in pairs(uconfig.struct.files) do
-        utils.touch(base .. file)
+        utils.touch(base .. "/" .. file)
     end
 end
 
---- Create symlinks to repos.
--- @param id task id
-local function link_repos(repobase, taskbase)
-    for _, repo in pairs(uconfig.repos) do
-        local target = repobase .. repo.name
-        local linkname = taskbase .. repo.name
-        utils.link(target, linkname)
-    end
-end
-
--- Private functions: end --
-
--- Public functions: start --
-
---- Create task filesystem structure.
--- @param id task id
--- @return on success - true
--- @return on failure - false
+---Create task filesystem structure.
+---@param envname string
+---@param id string
+---@return boolean
 function struct.create(envname, id)
     uconfig = config.uget(envname)
     local dirname = utils.genname(envname, id)
     local taskdir = core.struct.tasks.path .. dirname
-    local notedir = core.struct.tasks.path .. dirname .. "/notes/"
-    local repodir = core.struct.tasks.path .. dirname .. "/repos/"
 
-    utils.mkdir(notedir)
     utils.mkdir(taskdir)
-    utils.mkdir(repodir)
-
     create_dirs(taskdir)
-    create_files(notedir)
-    link_repos(core.struct.code.path, repodir)
+    create_files(taskdir)
     return true
 end
 
---- Delete task filesystem structure.
--- @param id task id
--- @return on success - true
--- @return on failure - false
+---Delete task filesystem structure.
+---@param envname string
+---@param id string
+---@return boolean
 function struct.delete(envname, id)
     local dirname = utils.genname(envname, id)
     local taskdir = core.struct.tasks.path .. "/" .. dirname
     return utils.rm(taskdir)
 end
 
---- Rename task dir.
--- @param oldid old task id
--- @param newid new task id
--- @return on success - true
--- @return on failure - false
+---Rename task dir.
+---@param envname string
+---@param oldid string
+---@param newid string
+---@return boolean
 function struct.rename(envname, oldid, newid)
     local old_dir = core.struct.units.path .. utils.genname(envname, oldid)
     local new_dir = core.struct.units.path .. utils.genname(envname, newid)
     return utils.rename(old_dir, new_dir) == 0
 end
-
--- Public functions: end --
 
 return struct
