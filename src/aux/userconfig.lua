437cfcd41a857b36578a750c26754b01de06c2bd
roach <qcockroach6@gmail.com>
2024-07-17 23:54:12 +0300

    aux/userconfig: Fix default values

--- src/aux/userconfig.lua
+++ src/aux/userconfig.lua
@@ -6,21 +6,24 @@ local utils = require("aux.utils")
 
 local uconfig = {}
 
-local default_repos = {}
-local default_branchpatt = "TYPE/ID_DESC_DATE"
-local default_commitpatt = "[ID] PART: MSG"
-local default_struct = {
+local default_plugin_struct = {
     dirs = {},
     files = {},
 }
+local default_plugin_git = {
+    repos = {},
+    dirbase = ".",
+    branchpatt = "TYPE/ID_DESC_DATE",
+    commitpatt = "[ID] PART: MSG",
+}
+local default_plugin_make = {
+    commands = {}
+}
+
 local default_env = {
-    repos = default_repos,
-    struct = {
-        dirs = {},
-        files = {},
-    },
-    branchpatt = default_branchpatt,
-    commitpatt = default_commitpatt,
+    struct = default_plugin_struct,
+    git = default_plugin_git,
+    make = default_plugin_make,
 }
 
 local function find_config_file(fname)
@@ -55,16 +58,23 @@ local config = json.decode(read_file(fconf))
 
 local function config_load()
     for envname, _ in pairs(config) do
-        config[envname].repos = config[envname].repos or default_repos
-
-        config[envname].struct = config[envname].struct or default_struct
+        -- plugin: struct
+        config[envname].struct = config[envname].struct or default_plugin_struct
         config[envname].struct.dirs = config[envname].struct.dirs or {}
         config[envname].struct.files = config[envname].struct.files or {}
 
-        config[envname].branchpatt = config[envname].branchpatt
-            or default_branchpatt
-        config[envname].commitpatt = config[envname].commitpatt
-            or default_commitpatt
+        -- plugin: git
+        config[envname].git = default_plugin_git
+        config[envname].git.repos = config[envname].repos
+            or default_plugin_git.repos
+        config[envname].git.branchpatt = config[envname].branchpatt
+            or default_plugin_git.branchpatt
+        config[envname].git.commitpatt = config[envname].commitpatt
+            or default_plugin_git.commitpatt
+
+        -- plugin: make
+        config[envname].make = default_plugin_make
+        config[envname].make.commands = default_plugin_make.commands
     end
 end
 
