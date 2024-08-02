20c1d7d44d2e88d1c8fd16fa1f94e1be72903ddf
Amonov Hurshed <amonov@wimark.com>
2024-08-03 02:10:40 +0300

    builtin/init: Add getopt functionality

--- src/builtin/init.lua
+++ src/builtin/init.lua
@@ -1,6 +1,16 @@
 local core = require("core.core")
+local getopt = require("posix.unistd").getopt
 
 local function tman_init()
+    local fbase
+    local optstr = "b:"
+
+    for optopt, optarg, optind in getopt(arg, optstr) do
+        if optopt == "?" then
+            return core.die(1, "unrecognized option", arg[optind - 1])
+        end
+    end
+
     if not core.create() then
         core.die(1, "could not create core structure", "init")
     end
