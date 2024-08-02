f2bc69681ae39bf0afa76e2a63f3cffe8945ca2d
Amonov Hurshed <amonov@wimark.com>
2024-08-03 02:13:19 +0300

    builtin/cfg: Rename local variable

--- src/builtin/cfg.lua
+++ src/builtin/cfg.lua
@@ -6,16 +6,16 @@ local config = require("aux.config")
 local function tman_config()
     local optstr = "b:i:s"
     local fshow = true -- default option
-    local fbase, finstall
-    local vbase, vinstall
+    local o_prefix, finstall
+    local v_prefix, vinstall
 
     for optopt, optarg, optind in getopt(arg, optstr) do
         if optopt == "?" then
             core.die(1, "unrecognized option", arg[optind - 1])
         end
         if optopt == "b" then
-            fbase = true
-            vbase = optarg
+            o_prefix = true
+            v_prefix = optarg
         elseif optopt == "i" then
             finstall = true
             vinstall = optarg
@@ -24,10 +24,10 @@ local function tman_config()
         end
     end
 
-    if fbase then
+    if o_prefix then
         -- roachme: check that vbase prefix directory can be created.
         --          convert to correct value if it's "." and has "~/"
-        config.sset("prefix", vbase)
+        config.sset("prefix", v_prefix)
     elseif finstall then
         print("set install value (under development)", vinstall)
     elseif fshow then
