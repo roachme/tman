f77173945de1b0171dbb5a5d03cddc68e909dbe5
Amonov Hurshed <amonov@wimark.com>
2024-11-13 01:23:22 +0300

    scr: Add module config to parse config file

--- src/core/config.c
+++ src/core/config.c
@@ -1,17 +1,63 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include "config.h"
 
-static char *fconfig = NULL;
-static char *dconfig = NULL;
+struct config config;
 
-/*
-    CONFIG = ~/.config/tman/sys.conf
-    DCONFIG = ~/.config/tman
-    HOOKLS = "list tag author"
-    HOOKLS = "list tag author"
-*/
+static int parsepath(char *path)
+{
+    const char *delim = " =\n";
+    strcpy(path, strtok(NULL, delim));
+    return 0;
+}
+
+static int parsehook(const char *hookname, struct hook *hookcmd)
+{
+    const char *delim = " =\n";
+    strcpy(hookcmd->hook, hookname);
+    strcpy(hookcmd->cmd, strtok(NULL, delim));
+    strcpy(hookcmd->pgname, strtok(NULL, delim));
+    strcpy(hookcmd->pgncmd, strtok(NULL, delim));
+    return 0;
+}
+
+int parseconf(const char *fname)
+{
+    char line[BUFSIZ];
+    char *token = NULL;
+    struct hook *hook;
+    const char *delim = " =\n";
+    FILE *fp = fopen(fname, "r");
+
+    if (!fp) {
+        fprintf(stderr, "could not open config file\n");
+        return 1;
+    }
+
+    while (fgets(line, BUFSIZ, fp) != NULL) {
+        token = strtok(line, delim);
+        if (!token || strlen(token) == 0 || token[0] == '\n' || token[0] == '#')
+            continue;
+
+        hook = &config.hooks[config.hooknum++];
+        if (strcmp(token, "TMANBASE") == 0)
+            parsepath(config.base);
+        else if (strcmp(token, "TMANPGNINS") == 0)
+            parsepath(config.pgnins);
+        else if (strcmp(token, "HOOKCMD") == 0)
+            parsehook(token, hook);
+        else if (strcmp(token, "HOOKCAT") == 0)
+            parsehook(token, hook);
+        else if (strcmp(token, "HOOKLIST") == 0)
+            parsehook(token, hook);
+        else {
+            fprintf(stderr, "not found %s: unknown variable\n", token);
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int findconfig()
 {
--- src/core/config.h
+++ src/core/config.h
@@ -1,6 +1,24 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+extern struct config config;
 
+struct hook {
+    char hook[10];
+    char cmd[10];
+    char cmdopt[10];
+    char pgname[10];
+    char pgncmd[10];
+    char pgnopt[10];
+};
+
+struct config {
+    int hooknum;
+    char base[100];
+    char pgnins[100];
+    struct hook hooks[10];
+};
+
+int parseconf(const char *fname);
 
 #endif
