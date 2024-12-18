b29e46ec1da1c772b1c4595d7cfe312ead516577
Amonov Hurshed <amonov@wimark.com>
2024-12-18 16:12:47 +0300

    cli/prev: Update command prev error message

--- cli/prev.c
+++ cli/prev.c
@@ -1,12 +1,34 @@
 #include "prev.h"
 #include "../src/tman.h"
 
+static int tman_cli_prev_usage(void)
+{
+    const char *cmd = "sync";
+    printf("Usage: %s %s [OPTION]\n", PROGRAM, cmd);
+    printf("Try '%s help %s' for more info.\n", PROGRAM, cmd);
+    return TMAN_OK;
+}
+
 int tman_cli_prev(int argc, char **argv)
 {
-    int status;
-    char *errfmt;
+    char c, *errfmt;
+    int showhelp, status;
 
+    showhelp = FALSE;
     errfmt =  "cannot switch: %s";
+    while ((c = getopt(argc, argv, ":h")) != -1) {
+        switch (c) {
+            case 'h':
+                showhelp = TRUE; break;
+            case ':':
+                return elog(1, "option `-%c' requires an argument", optopt);
+            default:
+                return elog(1, "invalid option `%c'", optopt);
+        }
+    }
+
+    if (showhelp == TRUE)
+        return tman_cli_prev_usage();
     if ((status = tman_id_prev()) != TMAN_OK)
         return elog(1, errfmt, tman_get_errmsg());
     return tman_pwd();
