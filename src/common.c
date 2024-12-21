e99e729a5e7f73df10954277481117ab7ffa8572
Amonov Hurshed <amonov@wimark.com>
2024-12-21 13:14:21 +0300

    src: Do minor updates on module task logic

--- src/common.c
+++ src/common.c
@@ -1,6 +1,11 @@
 #include <stdarg.h>
 #include <ctype.h>
+
+#include "tman.h"
 #include "common.h"
+
+static char *base = tmanfs.base;
+static char pathname[PATHSIZ + 1];
 
 char *formpath(char *dst, char *fmt, ...)
 {
@@ -49,6 +54,30 @@ int elog(int status, const char *fmt, ...)
     fprintf(stderr, "\n");
     va_end(arg);
     return status;
+}
+
+char *genpath_env(char *env)
+{
+    sprintf(pathname, "%s/%s", base, env);
+    return pathname;
+}
+
+char *genpath_full(char *env, char *id)
+{
+    sprintf(pathname, "%s/%s/%s", base, env, id);
+    return pathname;
+}
+
+char *genpath_col(char *env, char *id)
+{
+    sprintf(pathname, "%s/%s/%s/.tman/col", base, env, id);
+    return pathname;
+}
+
+char *genpath_unit(char *env, char *id)
+{
+    sprintf(pathname, "%s/%s/%s/.tman/unit", base, env, id);
+    return pathname;
 }
 
 /*
