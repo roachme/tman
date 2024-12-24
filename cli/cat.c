9a23fd38b8684d61cb5efc28a21c62e0c996a527
Amonov Hurshed <amonov@wimark.com>
2024-12-24 04:19:56 +0300

    cli/cat: Unify logic for current task ID and bunch of 'em

--- cli/cat.c
+++ cli/cat.c
@@ -3,14 +3,6 @@
 #include "cat.h"
 #include "cli.h"
 
-static int tman_cli_cat_usage(void)
-{
-    const char *cmd = "cat";
-    printf("Usage: %s %s [OPTION]... [ID]..\n", PROGRAM, cmd);
-    printf("Try '%s help %s' for more info.\n", PROGRAM, cmd);
-    return TMAN_OK;
-}
-
 static int pretty_cat(tman_ctx_t *ctx, char *env, char *id, char *key)
 {
     int i, status;
@@ -33,17 +25,15 @@ static int pretty_cat(tman_ctx_t *ctx, char *env, char *id, char *key)
     return TMAN_OK;
 }
 
-// TODO: Find a good error message in case option fails.  */
 int tman_cli_cat(int argc, char **argv, tman_ctx_t *ctx)
 {
     char c;
-    int status;
+    int i, status;
     struct tman_cli_cat_opt opt = { .env = NULL, .help = 0, .force = 0, };
 
     while ((c = getopt(argc, argv, ":e:fhk")) != -1) {
         switch (c) {
             case 'e': opt.env = optarg; break;
-            case 'h': opt.help = 1; break;
             case 'f': opt.force = 1; break;
             case 'k': opt.key = optarg; break;
             case ':':
@@ -53,12 +43,11 @@ int tman_cli_cat(int argc, char **argv, tman_ctx_t *ctx)
         }
     }
 
-    if (opt.help == 1)
-        return tman_cli_cat_usage();
-
-    for (int i = optind; i < argc; ++i)
-        status = pretty_cat(ctx, opt.env, argv[i], opt.key);
-
-    /* if no arguments passed cat current task (if any) */
-    return optind < argc ? status : pretty_cat(ctx, opt.env, NULL, opt.key);
+    i = optind;
+    do {
+        if ((status = pretty_cat(ctx, opt.env, argv[i], opt.key)) != TMAN_OK)
+            return status;
+        ++i;
+    } while (i < argc);
+    return status;
 }
