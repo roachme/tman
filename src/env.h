3e2fbb77a4f15393f332a34ba68d3ef7e30f245b
Amonov Hurshed <amonov@wimark.com>
2024-12-30 00:37:55 +0300

    src/env: Delete private macros from API header

--- src/env.h
+++ src/env.h
@@ -1,16 +1,12 @@
 #ifndef TMAN_ENV_H
 #define TMAN_ENV_H
 
-#define CENV        0  /* index of current env */
-#define PENV        1  /* index of previous env */
-#define NENV        2  /* number of 'special' environments */
 #define ENVSIZ      20 /* maximum size of environment name */
-#define NENVITEM    1  /* number of environment items per line */
 
 int env_init(char *fstate);
 int env_reset(void);
-char *env_getcurr();
-char *env_getprev();
+char *env_getcurr(void);
+char *env_getprev(void);
 int env_delcurr(void);
 int env_delprev(void);
 int env_swapenvs(void);
