d6357d48fd2f2d260b7ed943eeef94cf521f3f82
Amonov Hurshed <amonov@wimark.com>
2025-01-08 01:36:06 +0300

    src/limit: Add TODO comment

--- src/limit.h
+++ src/limit.h
@@ -5,10 +5,10 @@
 #ifndef LIMIT_H
 #define LIMIT_H
 
+/* roachme: delete these constrains */
+
 /* Config file limits */
 #define NHOOKS      10 /* max number of hooks */
 #define NCOLUMNS    10 /* max number of columns per environment */
-
-#define NTASKS      50 /* max number of tasks per environment */
 
 #endif
