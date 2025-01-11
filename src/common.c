e8c13f1f1879ea430ec170f281e3df86d87c21b1
Amonov Hurshed <amonov@wimark.com>
2025-01-11 17:10:50 +0300

    src/common: Delete unused functions

--- src/common.c
+++ src/common.c
@@ -16,37 +16,6 @@ char *formpath(char *dst, char *fmt, ...)
     return dst;
 }
 
-/*
-    @return 0 - failed
-    @return 1 - ok
-    else if (!_chkid(id)) - failed
-
-    The isalnum() and isalnum_l() functions shall return non-zero
-    if c is an alphanumeric character; otherwise, they shall
-    return 0.
-*/
-/* roach: depricated, tho module hook.c use it.  */
-int _chkid(char *id)
-{
-    if (!isalnum(*id++))
-        return 0;
-    for (; *id; ++id)
-        if (!(isalnum(*id) || *id == '_' || *id == '-'))
-            return 0;
-    return isalnum(*--id);
-}
-
-/* roach: depricated, tho module hook.c use it.  */
-int _chkenv(char *env)
-{
-    if (!isalnum(*env++))
-        return 0;
-    for (; *env; ++env)
-        if (!(isalnum(*env) || *env == '_' || *env == '-'))
-            return 0;
-    return isalnum(*--env);
-}
-
 int elog(int status, const char *fmt, ...)
 {
     va_list arg;
