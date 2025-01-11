2fba9e5a38a1465d0a5ff9dd0c1daca3871ef10d
Amonov Hurshed <amonov@wimark.com>
2025-01-11 17:07:34 +0300

    src/osdep: Add OS dependency stuff

--- src/osdep.c
+++ src/osdep.c
@@ -3,6 +3,8 @@
 
 #include "osdep.h"
 
+#ifdef __linux__
+
 BOOL MKDIR(char *path)
 {
     char cmd[BUFSIZ + 1];
@@ -44,3 +46,69 @@ BOOL ISDIR(char *fname)
     sprintf(cmd, "test -d %s", fname);
     return system(cmd) == EXIT_SUCCESS;
 }
+
+#elif __APPLE__
+
+BOOL MKDIR(char *path)
+{
+    return 0;
+}
+
+BOOL RMDIR(char *path)
+{
+    return 0;
+}
+
+BOOL MOVE(char *src, char *dst)
+{
+    return 0;
+}
+
+BOOL TOUCH(char *path)
+{
+    return 0;
+}
+
+BOOL ISFILE(char *fname)
+{
+    return 0;
+}
+
+BOOL ISDIR(char *fname)
+{
+    return 0;
+}
+
+#elif WIN32 || __MINGW32__
+
+BOOL MKDIR(char *path)
+{
+    return 0;
+}
+
+BOOL RMDIR(char *path)
+{
+    return 0;
+}
+
+BOOL MOVE(char *src, char *dst)
+{
+    return 0;
+}
+
+BOOL TOUCH(char *path)
+{
+    return 0;
+}
+
+BOOL ISFILE(char *fname)
+{
+    return 0;
+}
+
+BOOL ISDIR(char *fname)
+{
+    return 0;
+}
+
+#endif
