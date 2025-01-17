7c1de15bf2082e3eba3eacf1fbb39180f1e0333d
Amonov Hurshed <amonov@wimark.com>
2025-01-17 04:18:24 +0300

    src/tman: Add enviroment and task ID for command sync

--- cli/sync.c
+++ cli/sync.c
@@ -3,16 +3,27 @@
 // TODO: Find a good error message in case option fails.  */
 int tman_cli_sync(int argc, char **argv, tman_ctx_t *ctx)
 {
-    char c, *errfmt;
-    int showhelp, status;
-    struct tman_id_sync_opt opt;
+    char c, *errfmt, *env;
+    int i, force, quiet, showhelp, status;
+    struct tman_id_sync_opt opt = {
+        .doswitch = TRUE,
+    };
 
-    showhelp = FALSE;
-    errfmt =  "cannot sync: %s";
-    while ((c = getopt(argc, argv, ":h")) != -1) {
+    env = NULL;
+    force = quiet = showhelp = FALSE;
+    errfmt =  "cannot sync '%s': %s";
+    while ((c = getopt(argc, argv, ":e:fhnq")) != -1) {
         switch (c) {
+            case 'e':
+                env = optarg; break ;
+            case 'f':
+                force = TRUE; break ;
             case 'h':
                 showhelp = TRUE; break;
+            case 'n':
+                opt.doswitch = FALSE; break;
+            case 'q':
+                quiet = TRUE; break ;
             case ':':
                 return elog(1, "option `-%c' requires an argument", optopt);
             default:
@@ -22,7 +33,16 @@ int tman_cli_sync(int argc, char **argv, tman_ctx_t *ctx)
 
     if (showhelp == TRUE)
         return help_usage("sync");
-    if ((status = tman_id_sync(ctx, &opt)) != TMAN_OK)
-        return elog(status, errfmt, tman_strerror());
-    return tman_pwd();
+
+    i = optind;
+    do {
+        if ((status = tman_id_sync(ctx, env, argv[i], &opt)) != TMAN_OK) {
+            if (quiet == FALSE)
+                elog(status, errfmt, argv[i], tman_strerror());
+            if (force == TRUE)
+                break;
+        }
+    } while (++i < argc);
+
+    return opt.doswitch && status == TMAN_OK ? tman_pwd() : status;
 }
