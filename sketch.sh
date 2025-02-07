4b2c223b9376e570c0cd5cf117dc4d90f5d2c6b6
Amonov Hurshed <amonov@wimark.com>
2025-02-07 13:55:29 +0300

    tman: Delete unused script

--- sketch.sh
+++
@@ -1,9 +0,0 @@
-tags=( env_init env_reset env_getcurr env_getprev env_delcurr env_delprev env_swapenvs env_addcurr env_exists env_isvalid env_iscurr env_isprev)
-
-
-
-for tag in ${tags[@]}; do
-    SRC="$tag"
-    DST="$(echo $tag | sed 's/env/prj/g')"
-    find . -name '*.[h,c]' | xargs sed -i "s/$SRC/$DST/g"
-done
