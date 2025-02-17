04a06eae3d37af535d67d1299ba19f10e463978f
roach <qcockroach@gmail.com>
2025-02-17 14:29:40 +0300

    cli/chk: Add comment for future development

--- cli/chk.c
+++ cli/chk.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include "chk.h"
 
+/*
+    What could be fixed with no user data lost.
+Task:
+    1. Invalid task ID (rename to proper one)
+    2. Invaild column
+
+Project:
+    1. Invalid project name (rename to proper one)
+    3. Invalid toggle in state file
+*/
+
 int tman_cli_chk(int argc, char *argv)
 {
     printf("tman_cli_chk: check tman health\n");
