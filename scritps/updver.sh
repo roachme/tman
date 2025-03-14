3f13ed8b3375ef0b1e0085e42adc3ab92b564e99
roach <qcockroach@gmail.com>
2025-03-14 16:06:43 +0300

    refactor(scritps): Refactor logic for script updver.sh

--- scritps/updver.sh
+++ scritps/updver.sh
@@ -1,31 +1,58 @@
 #!/usr/bin/env bash
 
-set -e
-
-NEWVERSION="$1"
-
-if [ -z "$NEWVERSION" ]; then
-    echo "Usage: updver.sh NEWVERSION"
-    exit 1
-fi
-
-
 #TODO: check NEWVERSION value. Pattern: v<0-255>.<0-255>.<0-255>.
 #TODO: check NEWVERSION is greater than previous and does not exist already
 #TODO: update CHANGELOG: set new version and date
 
-git checkout master
 
-sed -i "/#define VERSION/s/\(#define VERSION\s*\).*/\1\"$NEWVERSION\"/" lib/common.h
-git add lib/common.h
-git commit -m "tman: Bump version to $NEWVERSION"
+VERSION="$1"
+# TODO: regex does not check number limits 0 < VERSION <= 255
+VERSION_PATTERN="v[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}"
 
-git tag "$NEWVERSION"
-git tag --delete latest
-git tag latest "$NEWVERSION"
+function usage()
+{
+    echo "Usage: updver.sh VERSION"
+    echo "VERSION pattern v[0-255].[0-255].[0-255]"
+    return 0
+}
 
-git push origin "$NEWVERSION"
-git push origin latest --force # overwrite previous latest tag
+function die()
+{
+    usage
+    exit 1
+}
 
-git checkout develop
-git rebase master
+function update_master()
+{
+    if ! git checkout --quiet master; then
+        echo "Current branch has uncommited changes. Cannot switch to master"
+        exit 1
+    fi
+
+    sed -i "/#define VERSION/s/\(#define VERSION\s*\).*/\1\"$NEWVERSION\"/" lib/common.h
+    git add lib/common.h
+    git commit -m "tman: Bump version to $NEWVERSION"
+
+    echo
+    echo "Update: CHANGELOG.md"
+}
+
+function update_develop()
+{
+    return 0
+}
+
+function update_remote()
+{
+    return 0
+}
+
+if [ -z "$VERSION" ]; then
+    echo "updver: Version is missing"
+    die
+elif ! echo "$VERSION" | grep -wq "$VERSION_PATTERN"; then
+    echo "updver: Invalid version"
+    die
+fi
+
+update_master
