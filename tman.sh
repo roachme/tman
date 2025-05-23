fb1c1558ff106f921c88d5db892b9823f890281f
Amonov Hurshed <qcockroach6@gmail.com>
2025-05-24 01:46:15 +0300

    tman.sh: Minor update on output parsing

--- tman.sh
+++ tman.sh
@@ -8,7 +8,7 @@ function tman()
 
     # Check that output contains tag. If so then interpret
     # it as a path not an output.
-    if [ -n "$(echo "$output" | grep -E "^$PATHTAG")" ]; then
+    if echo "$output" | grep -qE "^$PATHTAG"; then
         cd "${output/$PATHTAG/}" || return 1
     elif [ -n "$output" ]; then
         echo "$output"
